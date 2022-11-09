acda93fc46181d1413eac2eaa5414a4aa4ac56a5
angel <shauangel2000@gmail.com>
2022-11-09 16:18:09 +0800

    display topic coherence score

--- topic_coherent_score.py
+++ topic_coherent_score.py
@@ -1,4 +1,6 @@
 import json
+import re
+from gensim.models import EnsembleLda
 
 path = "data/exp2/draw/t_5/"
 file = "test_result_draw.json"
@@ -8,4 +10,8 @@ if __name__ == "__main__":
         result = json.load(f)
         f.close()
 
-
+    for t in result[4]['050']:
+        #print(t[1])
+        words = re.findall(r'\*"(.*?)"', t[1])
+        print(words)
+    #

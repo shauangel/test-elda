86929f05c35a6018cd1047c9ef64c29ffbf00e5c
angel <shauangel2000@gmail.com>
2022-11-16 10:09:47 +0800

    update

--- c_test.py
+++ c_test.py
@@ -1,14 +1,14 @@
 from nltk.corpus import brown
 from TextAnalyze import TextAnalyze
 
-from gensim.models import Lda
+from gensim.models import LdaModel
 from gensim.corpora.dictionary import Dictionary
 from gensim.models.coherencemodel import CoherenceModel
 
 class LDA():
     def __init__(self, path):
         try:
-            Lda.load(path)
+            LdaModel.load(path)
         except:
             print("no such model...")
 
@@ -16,7 +16,7 @@ class LDA():
         self._RawText = text
         self._Dict = Dictionary(text)
         self._Corp = [self._Dict.doc2bow(t) for t in text]
-        self.model = Lda(corpus=self._Corp, id2word=self._Dict, num_topics=topic_num)
+        self.model = LdaModel(corpus=self._Corp, id2word=self._Dict, num_topics=topic_num)
 
     def getModel(self):
         return self.model
@@ -24,17 +24,17 @@ class LDA():
 
 if __name__ == "__main__":
     ## Step 1.
+    print("step1,")
     news = [file for file in brown.fileids() if 'news' in brown.categories(file)]
     word_list = [brown.words(file) for file in news]
 
+
     ## Step 2.
+    print("step2,")
     analyzer = TextAnalyze()
     text = [analyzer.contentPreProcess(" ".join(l))[0] for l in word_list]
 
-    print(text[0])
+    print(len(text))
 
-    ##Step 3.
-    lda = LDA(text, 5).getModel()
-    for t in lda.print_topics(-1):
-        print(t)
+
 

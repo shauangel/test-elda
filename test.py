c283665d8fae6e4174d62b132f4ae54f393189e4
angel <shauangel2000@gmail.com>
2022-11-16 09:45:15 +0800

    update

--- test.py
+++ test.py
@@ -2,13 +2,11 @@ import string
 import nltk
 from nltk.corpus import brown
 from TextAnalyze import TextAnalyze
-stopwords = nltk.corpus.stopwords.words('english')
 
 news = [file for file in brown.fileids() if 'news' in brown.categories(file)]
 word_list = [brown.words(file) for file in news]
-corpus = [[w.lower() for w in l if w not in string.punctuation and w not in stopwords] for l in word_list]
+corpus = [ analyzer.contentPreProcess(" ".join(test))for l in word_list]
 
 analyzer = TextAnalyze()
 test = word_list[0]
 tokens, doc = analyzer.contentPreProcess(" ".join(test))
-print(tokens)

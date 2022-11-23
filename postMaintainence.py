bdbae9f633ce548cd46acd8fe16eded0fe959e76
angel <shauangel2000@gmail.com>
2022-11-23 08:15:48 +0800

    update tag generator

--- postMaintainence.py
+++ postMaintainence.py
@@ -2,6 +2,7 @@ import _db
 from StackData import StackData
 from TextAnalyze import TextAnalyze
 import csv
+import re
 
 
 # --> load data from db <--
@@ -11,14 +12,30 @@ def load_data():
     return [i for i in parsed]
 
 
-def generate_tags(text_list):
+def generate_tags(text_list, corpus_file, result_file):
     t_a = TextAnalyze()
     corpus = [t_a.contentPreProcess(i)[0] for i in text_list]
-    with open('corpus.csv', 'w', newline='') as csvfile:
+    with open( corpus_file+'.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         for article in corpus:
             writer.writerow(article)
-    t_a.eLDATopicModeling(corpus, 50, 5)
+        csvfile.close()
+    model = t_a.eLDATopicModeling(corpus, 100, 5)
+    tokens = []
+    topics = []
+    for t in model.print_topics(-1):
+        words = re.findall(r'\*"(.*?)"', t[1])
+        p = re.findall(r'(\d*.\d*?)\*', t[1])
+        tokens += words
+        distribution_dict = {words[i]:p[i] for i in range(len(words))}
+        topics.append(distribution_dict)
+        print(distribution_dict)
+    tokens = list(dict.fromkeys(tokens))
+    with open( result_file+'.csv', 'w', newline='') as csvfile:
+        dict_writer = csv.DictWriter(csvfile, fieldnames=tokens)
+        for t in topics:
+            writer.writerow(t)
+    return model
 
 
 # --> parse more posts <--
